672ad33dc452cf677c9fdf4b6df98f3f7104a223
RushenSamodya <114132773+RushenSamodya@users.noreply.github.com>
2023-03-05 11:51:20 +0530

    Completed the Horizintal Navbar

--- crawlee_1/src/App.js
+++ crawlee_1/src/App.js
@@ -1,12 +1,12 @@
 
-import Navbar from './Navbar.js';
+
 import './App.css';
 import {Routes, Route} from 'react-router-dom';
-import Home from './Home';
-import About from './About.js';
-import Courses from './Courses.js';
-import Login from './Login.js';
-import Signup from './Signup.js';
+import Home from './Pages/Home';
+import About from './Pages/About.js';
+import Courses from './Pages/Courses.js';
+import Login from './Pages/Login.js';
+import Signup from './Pages/Signup.js';
 import NavbarHorizontal from './NavbarHorizontal.jsx';
 
 //import TopNav2 from './TopNav2.jsx';
@@ -20,11 +20,11 @@ function App() {
         <Sidebar/> */}
         <NavbarHorizontal/>
           <Routes>
-            <Route path='/' element ={<Home/>}/>
-            <Route path='courses' element ={<Courses/>}/>
-            <Route path='About' element ={<About/>}/>
-            <Route path='login' element ={<Login/>}/>
-            <Route path='sign-up' element ={<Signup/>}/>
+            <Route path='Pages/' element ={<Home/>}/>
+            <Route path='Pages/courses' element ={<Courses/>}/>
+            <Route path='Pages/About' element ={<About/>}/>
+            <Route path='Pages/login' element ={<Login/>}/>
+            <Route path='Pages/sign-up' element ={<Signup/>}/>
           </Routes>
        
     </div>
